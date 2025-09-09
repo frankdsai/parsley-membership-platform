60d457b5c3277bd39896c8a61146e6447444af81
Frank DAndrea <frank@sapienceai.co>
2025-09-09 19:23:29 +0000

    Add Firebase configuration

---
+++ src/firebase.ts
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSybtJuEHg557z9fgryFSuYMeQ8rKinYXEK4",
+  authDomain: "parsley-membership-platform.firebaseapp.com",
+  projectId: "parsley-membership-platform",
+  storageBucket: "parsley-membership-platform.firebasestorage.app",
+  messagingSenderId: "943856613574",
+  appId: "1:943856613574:web:60878b73636874cf82bf67",
+  measurementId: "G-VSTY5SSMZ4"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export default app;
